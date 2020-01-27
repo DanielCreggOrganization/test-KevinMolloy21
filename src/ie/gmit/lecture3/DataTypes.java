bf057391154c288f2d9565430178bba396d09cda
KevinMolloy21 <G00371838@GMIT.IE>
2020-01-27 17:47:12 +0000

    classes

---
+++ src/ie/gmit/lecture3/DataTypes.java
@@ -0,0 +1,23 @@
+package ie.gmit.lecture3;
+
+/**
+ * DataTypes
+ */
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+            System.out.println("Hello");
+                boolean isAlive = true;
+                byte age = 100;
+                char grade = 'A';
+                short population = 20;
+                int studentid = 12;
+
+                System.out.println(isAlive);
+                System.out.println(age);
+                System.out.println(grade);
+                System.out.println(population);
+                System.out.println(studentid);
+}               
+}
